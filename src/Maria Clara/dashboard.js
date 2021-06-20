3b28fb3b5887fc64818fe4232aa14983fa8aabce
mariaclarabethonico <clarabterra@gmail.com>
2021-06-20 17:18:57 -0300

    Edits

---
+++ src/Maria Clara/dashboard.js
@@ -0,0 +1,73 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        '0Kwh',
+        '50Kwh',
+        '100Kwh',
+        '150Kwh',
+        '200Kwh',
+        '250Kwh',
+        '300Kwh'
+      ],
+      datasets: [{
+        data: [
+          15339,
+          21345,
+          18483,
+          24003,
+          23489,
+          24092,
+          12034
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+},
+  options: {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: false
+      }
+    }]
+  },
+  legend: {
+    display: false
+  }
+}
+  })
+}) ()
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
